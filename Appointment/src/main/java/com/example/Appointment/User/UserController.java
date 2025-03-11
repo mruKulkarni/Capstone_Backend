72d845d1207711fd5a4483aa7ac3813cc91df25b
Dronanaik <dronanaik1133@gmail.com>
2025-03-11 15:39:45 +0530

    Added Registration

--- Appointment/src/main/java/com/example/Appointment/User/UserController.java
+++ Appointment/src/main/java/com/example/Appointment/User/UserController.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -17,4 +21,9 @@ public class UserController {
 	public User getUserByEmail(@PathVariable("email")String email){
 		return this.userService.getByEmail(email);
 	}
+	@PostMapping("/register")
+    public User registerUser(@Valid @RequestBody User user) {
+    	
+        return userService.createUser(user);
+    }
 }
---
+++ Appointment/src/main/java/com/example/Appointment/User/UserDTO.java
@@ -0,0 +1,27 @@
+package com.example.Appointment.User;
+
+public class UserDTO {
+	private String email;
+	private String password;
+	public UserDTO(String email, String password) {
+		super();
+		this.email = email;
+		this.password = password;
+	}
+	public UserDTO() {
+		super();
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
