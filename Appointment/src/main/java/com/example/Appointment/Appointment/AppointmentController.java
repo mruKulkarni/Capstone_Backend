5838dcbcd5ec545a0df9562a602437fd8a453621
shrinivedhaa <shrinivedhaa@gmail.com>
2025-03-12 15:19:17 +0530

    Appointment functionality

--- Appointment/src/main/java/com/example/Appointment/Appointment/AppointmentController.java
+++ Appointment/src/main/java/com/example/Appointment/Appointment/AppointmentController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/appointments")
+@CrossOrigin("*")
 public class AppointmentController {
 	@Autowired
 	private AppointmentService appointmentService;
--- Appointment/src/main/java/com/example/Appointment/Doctor/DoctorController.java
+++ Appointment/src/main/java/com/example/Appointment/Doctor/DoctorController.java
@@ -2,6 +2,7 @@ package com.example.Appointment.Doctor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/doctors")
+@CrossOrigin("*")
 public class DoctorController {
 	@Autowired
 	private DoctorService doctorService;
