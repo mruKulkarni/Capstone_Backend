b998d94c38ff948eb784e4b229548e30e9c6457d
Dronanaik <dronanaik1133@gmail.com>
2025-03-13 17:24:13 +0530

    Department Frontend added

--- Appointment/src/main/java/com/example/Appointment/Doctor/DoctorController.java
+++ Appointment/src/main/java/com/example/Appointment/Doctor/DoctorController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/doctors")
 @CrossOrigin("*")
-@CrossOrigin(origins = "http://localhost:4200")
 public class DoctorController {
 	@Autowired
 	private DoctorService doctorService;
