b6c90cf10a2f62a1ce8630e1fe8ffe6b68daed6f
shrinivedhaa <shrinivedhaa@gmail.com>
2025-03-20 00:02:48 +0530

    JUnit testing for Doctor package

--- Appointment/src/test/java/com/example/Appointment/Doctor/DoctorControllerTest.java
+++ Appointment/src/test/java/com/example/Appointment/Doctor/DoctorControllerTest.java
@@ -1,4 +1,4 @@
-package com.example.Appointment.Controller;
+package com.example.Appointment.Doctor;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
